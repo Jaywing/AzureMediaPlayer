505110d9671ae6215d5e1f7e4e4c12d716ff5af0
Gareth Wright <garpunkal@gmail.com>
2018-06-19 11:35:23 +0100

    init

---
+++ AzureMediaPlayer/azuremediaplayer.js
@@ -0,0 +1,22 @@
+﻿angular.module("umbraco")
+    .controller("Jaywing.AzureMediaPlayerController",
+    function ($scope) {
+
+        var engine = {
+            url: '',
+            update: null
+        };
+
+        if ($scope.model.value) {
+            if ($scope.model.value !== "") {
+                engine.url = $scope.model.value;
+            } else
+                engine.url = '';
+        }
+
+        engine.update = function () {
+            $scope.model.value = engine.url;
+        };
+
+        $scope.engine = engine;
+    });
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# AzureMediaPlayer
-Azure Media Player datatype
+# Umbraco AzureMediaPlayer
+
+A simple app_plugin for Umbraco that allows you to view Azure media items in the media library using the Azure Media Player embed. 
+
+### Screenshot
+![Screenshot](screenshot.png)
+
+ 
